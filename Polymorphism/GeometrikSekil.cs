82f050f1dc5cfd793b5fa8f1753b522cb9cb0a91
AbdullahVC <69648705+AbdullahVC@users.noreply.github.com>
2024-08-21 18:51:49 +0300

    Proje dosyası ekle.

---
+++ Polymorphism/GeometrikSekil.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    // Base class: BaseGeometrikSekil
+    class BaseGeometrikSekil
+    {
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+
+        public virtual double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    // Derived class: Kare
+    class Kare : BaseGeometrikSekil
+    {
+        public Kare(double kenar)
+        {
+            Genislik = kenar;
+            Yukseklik = kenar;
+        }
+    }
+
+    // Derived class: Dikdortgen
+    class Dikdortgen : BaseGeometrikSekil
+    {
+        public Dikdortgen(double genislik, double yukseklik)
+        {
+            Genislik = genislik;
+            Yukseklik = yukseklik;
+        }
+    }
+
+    // Derived class: DikUcgen
+    class DikUcgen : BaseGeometrikSekil
+    {
+        public DikUcgen(double genislik, double yukseklik)
+        {
+            Genislik = genislik;
+            Yukseklik = yukseklik;
+        }
+
+        public override double AlanHesapla()
+        {
+            return (Genislik * Yukseklik) / 2;
+        }
+    }
+
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Polymorphism 
+{
+class Program
+{
+    static void Main()
+    {
+        // Kare
+        Console.Write("Kare için kenar uzunluğunu giriniz: ");
+        double kareKenar = Convert.ToDouble(Console.ReadLine());
+        Kare kare = new Kare(kareKenar);
+        Console.WriteLine("Kare Alanı: " + kare.AlanHesapla());
+            Console.WriteLine("");
+
+        // Dikdörtgen
+        Console.Write("Dikdörtgen için genişlik giriniz: ");
+        double dikdortgenGenislik = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Dikdörtgen için yükseklik giriniz: ");
+        double dikdortgenYukseklik = Convert.ToDouble(Console.ReadLine());
+        Dikdortgen dikdortgen = new Dikdortgen(dikdortgenGenislik, dikdortgenYukseklik);
+        Console.WriteLine("Dikdörtgen Alanı: " + dikdortgen.AlanHesapla());
+            Console.WriteLine("");
+
+        // Dik Üçgen
+        Console.Write("Dik Üçgen için genişlik giriniz: ");
+        double dikUcgenGenislik = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Dik Üçgen için yükseklik giriniz: ");
+        double dikUcgenYukseklik = Convert.ToDouble(Console.ReadLine());
+        DikUcgen dikUcgen = new DikUcgen(dikUcgenGenislik, dikUcgenYukseklik);
+        Console.WriteLine("Dik Üçgen Alanı: " + dikUcgen.AlanHesapla());
+    }
+}
+}
